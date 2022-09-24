733ba2dc4674c466a2fd944332fbed59e22ea1f7
Behnam Sajadifar <beh.sajadi94@gmail.com>
2022-09-24 17:51:47 +0330

    styles(shared): improve code structure in shamsi date pipe

--- src/app/shared/pipes/shamsi-date.pipe.ts
+++ src/app/shared/pipes/shamsi-date.pipe.ts
@@ -9,21 +9,19 @@ export class ShamsiDatePipe implements PipeTransform {
    * and be more configurable. But since this is a test
    * project, we'll leave it at this basic implementation.
    *
-   * @param {(string | number)} value: A datetime-like string
-   * @returns {string}: Shamsi date string in YY/MM/DD format
+   * @param {(string | number)} value A datetime-like string
+   * @returns {string} Shamsi date string in YY/MM/DD format
    */
   transform(value: string | number): string {
     return this.transformToShamsi(value);
   }
 
   private transformToShamsi(value: string | number) {
-    return new Date(value)
-      .toLocaleString('fa-IR', {
-        year: '2-digit',
-        month: 'numeric',
-        day: '2-digit',
-        timeZone: 'Asia/Tehran',
-      })
-      .replace('،', ' -');
+    return new Date(value).toLocaleString('fa-IR', {
+      year: '2-digit',
+      month: 'numeric',
+      day: '2-digit',
+      timeZone: 'Asia/Tehran',
+    });
   }
 }
