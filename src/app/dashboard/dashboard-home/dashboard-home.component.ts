e8aa707a4a9993de0d54142a552370b7f2f100f5
Behnam Sajadifar <beh.sajadi94@gmail.com>
2022-09-24 17:43:03 +0330

    fix(filter-bar): fix filterbar routing issue

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
-  styleUrls: ['./dashboard-home.component.less']
+  styleUrls: ['./dashboard-home.component.less'],
 })
-export class DashboardHomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DashboardHomeComponent {
+  constructor() {}
 }
