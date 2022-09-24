6bd55c1ed024e38177acaf87a59d5d252614eb50
Behnam Sajadifar <beh.sajadi94@gmail.com>
2022-09-24 08:44:49 +0330

    feat(my-services): add header and basic styles

--- src/app/dashboard/pages/my-services-page/my-services-page.module.ts
+++ src/app/dashboard/pages/my-services-page/my-services-page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MyServicesPageRoutingModule } from './my-services-page-routing.module';
 import { MyServicesPageComponent } from './my-services-page.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [MyServicesPageComponent],
-  imports: [CommonModule, MyServicesPageRoutingModule],
+  imports: [CommonModule, MyServicesPageRoutingModule, NzIconModule, NzGridModule],
 })
 export class MyServicesPageModule {}
